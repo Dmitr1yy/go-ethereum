b27a15a9d922b0f8e061b9c16be0d80d6d006cb3
Dimka <141173731+Dmitr1yy@users.noreply.github.com>
2024-01-02 09:30:15 +0200

    Update params.go

--- beacon/params/params.go
+++ beacon/params/params.go
@@ -9,6 +9,10 @@
 // The go-ethereum library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS 
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
